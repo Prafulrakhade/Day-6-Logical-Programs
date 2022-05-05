bd4a88a1ee101b295b4db2496e99cfe4e91e9c37
Praful <prafulrakhade02@gmail.com>
2022-05-05 16:42:00 +0530

    [praful] Added Day 6 Logical Problems Program Files

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------Logical Problems------------");
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            PerfectNumber number = new PerfectNumber();
+            PrimeNumber prime = new PrimeNumber();
+            ReverseNumber reverse = new ReverseNumber();
+            SimulateStopwatch stopwatch=new SimulateStopwatch();
+        }
+    }
+}
